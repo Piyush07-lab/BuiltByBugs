dc51b608c75cd6471e51843a938ec0126c453658
PiyushMishra <piyushmishraone999@gmail.com>
2025-06-25 08:31:19 +0530

    feat: update assets and improve layout
    - Added new logo images: logo.png and logoClears.png.
    - Removed old logo image: OCDbugLogo.jpg.
    - Updated index.html to replace the old logo with a new text-based logo design.
    - Enhanced the main section layout and styling in style.css for better responsiveness and visual appeal.
    - Adjusted button styles and quick links for improved user experience.
    - Updated script.js to modify event handling for the middle toggle section.
    - Added package.json for development dependency management.

--- root/script.js
+++ root/script.js
@@ -1,6 +1,10 @@
 // Script file here
-document.querySelector(`.mainTwo`).addEventListener(`click`, function() {
-    this.classList.toggle(`expanded`);
+// document.querySelector(`.mainTwo`).addEventListener(`click`, function() {
+//     this.classList.toggle(`expanded`);
+// });
+
+document.getElementById("middleToggle").addEventListener("click", () => {
+    document.getElementById("middleToggle").classList.toggle("expanded");
 });
 
 // console.log(`Script is runnung!`);
