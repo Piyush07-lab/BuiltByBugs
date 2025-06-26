29838aa580b01914cd7eae9f936edd1886449b03
PiyushMishra <piyushmishraone999@gmail.com>
2025-06-26 23:21:07 +0530

    feat: enhance logo structure, add toggle functionality, and improve layout in mainTwo section

--- root/script.js
+++ root/script.js
@@ -7,4 +7,11 @@ document.getElementById("middleToggle").addEventListener("click", () => {
     document.getElementById("middleToggle").classList.toggle("expanded");
 });
 
+const mainTwo = document.getElementbyId("middleToggle");
+const toggleBar = document.getElementById("toggleBar");
+
+toggleBar.addEventListener("click", () => {
+    mainTwo.classList.toggle("expanded");
+});
+
 // console.log(`Script is runnung!`);
