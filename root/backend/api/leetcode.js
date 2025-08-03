a67d33cf3d13a0e363ea2ee626e2d7c73ed9768a
PiyushMishra <piyushmishraone999@gmail.com>
2025-08-04 01:09:55 +0530

    feat: add initial LeetCode tracker API implementation and configuration

--- root/backend/api/leetcode.js
+++ root/backend/api/leetcode.js
@@ -1,75 +1,81 @@
-//Leetcode tracker api
-// require("dotenv").config({ path: __dirname + "/.env" });
-// console.log("Session cookie:", process.env.LEETCODE_SESSION);
-// const https = require("https");
+// Leetcode tracker api
 
-// function getLeetCodeStats(username, callback) {
-//     const query = JSON.stringify({
-//         query: `
-//          query userProfile($username: String!) {
-//           matchedUser(username: $username) {
-//             username
-//             profile {
-//               reputation
-//               ranking
-//               starRating
-//             }
-//             submitStats: submitStatsGlobal {
-//               acSubmissionNum {
-//               difficulty
-//               count
-//               submissions
-//             }
-//           }
-//        }
-//      }
-//      `,
-//         variables: {
-//             username
-//         }
-//     });
+require("dotenv").config({ path: __dirname + "/.env" });
 
-//     const options = {
-//         hostname: "leetcode.com",
-//         path: "/graphql",
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//             "Content-Length": Buffer.byteLength(query),
-//             "Cookie": `LEETCODE_SESSION=${process.env.LEETCODE_SESSION}`,
-//             "Referer": "https://leetcode.com/",
-//             "Origin": "https://leetcode.com/",
-//             "User-Agent": "Mozilla/5.0 (Node.js)"
-//         }
-//     };
-
-//     const req = https.request(options, res => {
-//         let data = "";
-
-//         res.on("data", chunk => data += chunk);
-//         res.on("end", () => {
-//             try {
-//                 const result = JSON.parse(data);
-//                 console.log("LeetCode response:", result);
-//                 callback(null, result);
-//             } catch (err) {
-//                 callback(err, null);
-//             }
-//         });
-//     });
-
-//     req.on("error", err => callback(err, null));
-
-//     console.log("Sending request to LeetCode...");
-//     console.log("Headers:", options.headers);
-//     console.log("Body:", query);
+console.log("Session cookie:", process.env.LEETCODE_SESSION);
+const https = require("https");
 
 
+function getLeetCodeStats(username, callback) {
+    const query = JSON.stringify({
+        query: `
+         query userProfile($username: String!) {
+          matchedUser(username: $username) {
+            username
+            profile {
+              reputation
+              ranking
+              starRating
+            }
+            submitStats: submitStatsGlobal {
+              acSubmissionNum {
+              difficulty
+              count
+              submissions
+            }
+          }
+       }
+     }
+     `,
+        variables: {
+            username
+        }
+    });
 
-//     req.write(query);
-//     req.end();
-// }
 
-// module.exports = getLeetCodeStats;
+    const options = {
+        hostname: "leetcode.com",
+        path: "/graphql",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Content-Length": Buffer.byteLength(query),
+            "Cookie": `LEETCODE_SESSION=${process.env.LEETCODE_SESSION}`,
+            "Referer": "https://leetcode.com/",
+            "Origin": "https://leetcode.com/",
+            "User-Agent": "Mozilla/5.0 (Node.js)"
+        }
+    };
+
+
+    const req = https.request(options, res => {
+        let data = "";
+
+        res.on("data", chunk => data += chunk);
+        res.on("end", () => {
+            try {
+                const result = JSON.parse(data);
+                console.log("LeetCode response:", result);
+                callback(null, result);
+            } catch (err) {
+                callback(err, null);
+            }
+        });
+    });
+    
+
+    req.on("error", err => callback(err, null));
+
+    console.log("Sending request to LeetCode...");
+    console.log("Headers:", options.headers);
+    console.log("Body:", query);
+
+
+
+    req.write(query);
+    req.end();
+}
+
+module.exports = getLeetCodeStats;
 
 
