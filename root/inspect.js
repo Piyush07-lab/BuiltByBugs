521e33ec4a0f6c7d2f645261d3dab9054743890b
PiyushMishra <piyushmishraone999@gmail.com>
2026-06-21 09:55:38 +0530

    feat: implement API call inspection; add file reading, AST parsing, and fetch call traversal

--- root/inspect.js
+++ root/inspect.js
@@ -1,2 +1,37 @@
-// Inspection of files
 
+const fs = require("fs");
+const path = require("path");
+const parser = require("@babel/parser");
+const traverse = require("@babel/traverse").default;
+
+const filePath = path.join(
+    __dirname,
+    "frontend/js/API-fetch.js"
+);
+
+const code = fs.readFileSync(filePath, "utf8");
+
+const ast = parser.parse(code, {
+    sourceType: "module",
+    plugins: ["jsx"]
+});
+
+console.log("\n=== API CALLS ===\n");
+
+traverse(ast, {
+    CallExpression(path) {
+
+        const { callee } = path.node;
+
+        // fetch(...)
+        if (
+            callee.type === "Identifier" &&
+            callee.name === "fetch"
+        ) {
+            console.dir(path.node, {
+                depth: null,
+                colors: true
+            });
+        }
+    }
+});
